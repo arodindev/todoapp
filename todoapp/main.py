22c5615430b2da8e4abf3e9db1ad97a23ae0352c
arodindev <arodin.devv@gmail.com>
2024-08-17 13:46:33 +0200

    fix: allow all origins

--- todoapp/main.py
+++ todoapp/main.py
@@ -30,7 +30,7 @@ app.include_router(root_router)
 if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
